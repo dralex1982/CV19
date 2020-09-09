ac4cf2f044ad5db382d2fd29af9be7f17728accb
dralex1982 <dralex1982@gmail.com>
2020-09-09 22:50:21 +0300

    Добавляем сборку XamlBehaviorsWpf для использования интерактивных элементов в разметке

--- obj/Debug/netcoreapp3.1/App.g.i.cs
+++ obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -59,7 +59,7 @@ namespace CV19 {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/CV19;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CV19;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
